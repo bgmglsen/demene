8c4e06809560c87e893cf14c2434e5a845fd04b6
bgmglsen <76910702+bgmglsen@users.noreply.github.com>
2021-01-03 18:51:15 +0300

    Add files via upload

---
+++ src/desktop/core/state/MenuDurum.java
@@ -0,0 +1,25 @@
+package com.mygdx.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuDurum extends Durum {
+
+	@Override
+	public void render(SpriteBatch sb) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updute(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void elGiris() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
